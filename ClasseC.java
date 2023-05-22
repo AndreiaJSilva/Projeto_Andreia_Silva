fe7cd8304a941590fafccffe901f48e807bc2cc6
Andreia Silva <89694963+AndreiaJSilva@users.noreply.github.com>
2023-05-22 20:03:35 -0300

    Criacao da Classe C

---
+++ ClasseC.java
@@ -0,0 +1,18 @@
+public class ClasseC {
+  private String C1;
+  private int C2;
+
+  ClasseC(String x, int y){
+    C1 = x;
+    C2 = y;
+  }
+
+  public String MC1(){
+    return ("Método MC1 - ClasseC");
+  }
+
+  public String MC2(){
+    return ("Método MC2 - ClasseC");
+  }
+
+};
