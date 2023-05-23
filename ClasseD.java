62cfea87f667e7429cca3a1f2bee44e5204d8698
Andreia Silva <89694963+AndreiaJSilva@users.noreply.github.com>
2023-05-22 21:39:13 -0300

    Criacao da Classe D

---
+++ ClasseD.java
@@ -0,0 +1,19 @@
+public class ClasseD {
+  private String D1;
+  private double D2;
+
+  ClasseD(String x, double y){
+    D1 = x;
+    D2 = y;
+  }
+
+  public String MD1(String mensagem){
+    String aux = mensagem;
+    return (aux);
+  }
+
+  public String MD2(double x){
+    double aux = x + 20.5;
+    return ("Método MD2 - soma mais 20.5:" + aux);
+  }
+}
